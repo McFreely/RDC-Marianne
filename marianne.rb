e3ed5cdef5fb84f652c869abd193c74464f9b74d
McFreely <paulmcfreely@gmail.com>
2013-04-19 01:24:32 +0200

    CODE: Created the dir for the visitors facing web site; added basic visualisation for the results of the analysis

---
+++ marianne.rb
@@ -0,0 +1,20 @@
+require "rubygems"
+require "sinatra"
+require "mongoid"
+require "slim"
+
+Mongoid.load!("mongoid.yml")
+
+# The document model for the results of the analysis
+class Stat
+  include Mongoid::Document
+  field :title, type: String 
+  field :total_count, type: Integer
+  field :stat_positive, type: Integer
+  field :stat_negative, type: Integer
+end
+
+get '/' do
+  @stats = Stat.all
+  slim :index
+end
